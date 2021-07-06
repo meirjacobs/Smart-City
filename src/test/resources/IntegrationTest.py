920b4da8c0506f66b6362c8608dbd703b038e915
meirjacobs <jmjacob4@mail.yu.edu>
2021-07-06 12:32:39 -0400

    tiny change to IntegrationTest

--- src/test/resources/IntegrationTest.py
+++ src/test/resources/IntegrationTest.py
@@ -4,8 +4,8 @@ import requests
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        response = requests.get("https://3ef2nnls3c.execute-api.us-east-1.amazonaws.com/deployedStage")
-        print(response.status_code)
+        response = requests.get("https://3ef2nnls3c.execute-api.us-east-1.amazonaws.com/deployedStage", "id=1")
+        self.assertEqual(response.status_code, 200, "ERROR: statusCode = " + str(response.status_code))
 
 
 if __name__ == '__main__':
