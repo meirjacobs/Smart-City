19c2889f9df2f80b40d8cfc41dbe2834d11b2490
EphraimC <ephraim.crystal@gmail.com>
2021-07-19 09:55:59 -0400

    Report Issue and Find Issue pages successfully println all data inputted to them, renamed folder

--- SpringBoot/smartcity/smartcity/src/main/java/com/smartcity/smartcity/SmartCityController.java
+++ SpringBoot/smartcity/smartcity/src/main/java/com/smartcity/smartcity/SmartCityController.java
@@ -10,15 +10,17 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.List;
 import java.util.Scanner;
+
+import static org.apache.commons.io.FileUtils.readFileToByteArray;
+import static org.apache.tomcat.util.http.fileupload.FileUtils.*;
 
 @org.springframework.stereotype.Controller
 public class SmartCityController {
@@ -75,7 +77,7 @@ public class SmartCityController {
             return image;
         }
 
-        public void setImage(MultipartFile encodedImage) {
+        public void setImage(MultipartFile image) {
             this.image = image;
         }*/
 
@@ -88,10 +90,11 @@ public class SmartCityController {
         }*/
 
         public File getImage() {
+            //System.out.println(image.getAbsolutePath());
             return image;
         }
 
-        public void setImage(File encodedImage) {
+        public void setImage(File image) {
             this.image = image;
         }
 
@@ -104,20 +107,157 @@ public class SmartCityController {
         }
     }
 
-    @PostMapping("/report")
-    public String showPage(@ModelAttribute("data") PostData body) throws IOException {
+    public class GetData {
+        private String id;
+        private String problemType;
+        private String distance;
+        private String latitude;
+        private String longitude;
+        private String problemDescription;
+        private File image;
+        private String status;
+        //private LocalDateTime startTime;
+        //private LocalDateTime endTime;
+        private String startTime;
+        private String endTime;
 
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getProblemType() {
+            return problemType;
+        }
+
+        public void setProblemType(String problemType) {
+            this.problemType = problemType;
+        }
+
+        public String getDistance() {
+            return distance;
+        }
+
+        public void setDistance(String distance) {
+            this.distance = distance;
+        }
+
+        public String getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(String latitude) {
+            this.latitude = latitude;
+        }
+
+        public String getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(String longitude) {
+            this.longitude = longitude;
+        }
+
+        public String getProblemDescription() {
+            return problemDescription;
+        }
+
+        public void setProblemDescription(String problemDescription) {
+            this.problemDescription = problemDescription;
+        }
+
+        public File getImage() {
+            return image;
+        }
+
+        public void setImage(File image) {
+            this.image = image;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        /*public LocalDateTime getStartTime() {
+            return startTime;
+        }
+
+        public void setStartTime(LocalDateTime startTime) {
+            this.startTime = startTime;
+        }
+
+        public LocalDateTime getEndTime() {
+            return endTime;
+        }
+
+        public void setEndTime(LocalDateTime endTime) {
+            this.endTime = endTime;
+        }*/
+        public String getStartTime() {
+            return startTime;
+        }
+
+        public void setStartTime(String startTime) {
+            this.startTime = startTime;
+        }
+
+        public String getEndTime() {
+            return endTime;
+        }
+
+        public void setEndTime(String endTime) {
+            this.endTime = endTime;
+        }
+    }
+
+    @PostMapping("/report")
+    public String submitReport(@ModelAttribute("data") PostData body) throws IOException {
+        System.out.println("--REPORT--");
         System.out.println("Problem Type: " + body.getProblemType());
         System.out.println("Problem Description: " + body.getProblemDescription());
         System.out.println("Latitude: " + body.getLatitude());
         System.out.println("Longitude: " + body.getLongitude());
-        /*byte[] fileContent = Files.readAllBytes(body.getEncodedImage().toPath());
+        //byte[] fileContent = Files.readAllBytes(body.getImage());
+        /*byte[] fileContent = readFileToByteArray(body.getImage());
         String encodedString = Base64
                 .getEncoder()
                 .encodeToString(fileContent);
         System.out.println("Image: " + encodedString);*/
+        /*Reader fileReader = new FileReader(body.getImage());
+        int data = fileReader.read();
+        while(data != -1) {
+            //do something with data...
+            System.out.println("Data :" + data);
+
+            data = fileReader.read();
+        }
+        fileReader.close();*/
         System.out.println("Encoded Image: " + body.getImage());
+        //System.out.println("Encoded Image: " + body.getImage().getOriginalFilename());
         return "ReportPage";
+    }
+
+    @PostMapping("/find")
+    public String submitFind(@ModelAttribute("data") GetData body) {
+        System.out.println("--FIND--");
+        System.out.println("ID: " + body.getId());
+        System.out.println("Problem Type: " + body.getProblemType());
+        System.out.println("Problem Description: " + body.getProblemDescription());
+        System.out.println("Distance: " + body.getDistance());
+        System.out.println("Latitude: " + body.getLatitude());
+        System.out.println("Longitude: " + body.getLongitude());
+        System.out.println("Image: " + body.getImage());
+        System.out.println("Status: " + body.getStatus());
+        System.out.println("Start Time: " + body.getStartTime());
+        System.out.println("End Time: " + body.getEndTime());
+        return "FindPage";
     }
 
     @GetMapping("/report")
@@ -152,7 +292,8 @@ public class SmartCityController {
     }
 
     @GetMapping("/find")
-    public String getFindPage() {
+    public String getFindPage(Model model) {
+        model.addAttribute("data", new GetData());
         return "FindPage";
     }
 }
