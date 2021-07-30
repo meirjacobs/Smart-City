1299b6614676ddd82b0be376fa47d126ca65238f
meirjacobs <jmjacob4@mail.yu.edu>
2021-07-30 09:17:34 -0400

    testing

--- SpringBoot/smartcity/automation_batch.bat
+++ SpringBoot/smartcity/automation_batch.bat
@@ -5,6 +5,6 @@ cd smartcity
 call mvn install
 cd target
 docker pull ecrystal/smart_city_repository
-docker build -f Dockerfile .
+docker build -t ecrystal/smart_city_repository -f Dockerfile .
 docker run -p 8080:8080 -e API_URL=https://rpixnvd51i.execute-api.us-east-1.amazonaws.com/deployedStage ecrystal/smart_city_repository:latest
 cd ..\..
